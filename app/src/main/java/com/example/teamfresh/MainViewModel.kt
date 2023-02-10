6675172f38c325cdd4f0254a51b8e5a019baaed9
JangYouwon <wkddbdnjs81695@gmail.com>
2023-02-11 02:25:34 +0900

    splahScreeb

---
+++ app/src/main/java/com/example/teamfresh/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.teamfresh
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application):AndroidViewModel(application) {
+}
